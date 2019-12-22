5dd92cdb5b8c2fd4d661a542eaf0e5c633ab6cb8
Kenneth Walker <kwalkerdev@gmail.com>
2019-12-21 18:05:52 -0800

    Added some skeleton files for upcoming classes.

--- Rengage/src/Model.cpp
+++ Rengage/src/Model.cpp
@@ -6,7 +6,7 @@ namespace RenGageAPI
 	Model::Model()
 	{
 		using namespace Utilities;
-		LOG_TO_CONSOLE("Hello world from a Model!", Severity::Info);
+		//LOG_TO_CONSOLE("Hello world from a Model!", Severity::Info);
 	}
 
 
