e262cb8c55de1fd568b2a2c22fc01684fb088825
Kenneth Walker <kwalkerdev@gmail.com>
2023-02-23 16:49:41 -0800

    Started implementing shader load functions.

--- RenGage.Lib/inc/rengage.lib/shader_factory.h
+++ RenGage.Lib/inc/rengage.lib/shader_factory.h
@@ -1,15 +1,17 @@
 #pragma once
 #include "shader.h"
+#include "logger/logger_macros.h"
 #include <fstream>
 
 namespace rengage {
 	class ShaderFactory {
 	public:
 		~ShaderFactory() = default;
-		static std::unique_ptr<Shader> get_shader_from_file(const GLuint type, const std::string& filename);
-		static std::unique_ptr<Shader> get_shader_from_source(const GLuint type, const std::string& source);
+		static std::unique_ptr<Shader> load_shader_from_file(const GLuint type, const char* source);
+		static std::unique_ptr<Shader> load_shader_from_source(const GLuint type, const char* source);
 
 	private:
 		ShaderFactory() = default;
+		static GLuint compile_shader_source(const GLuint shader_id, const char* source);
 	};
 }
--- RenGage.Lib/src/shader_factory.cpp
+++ RenGage.Lib/src/shader_factory.cpp
@@ -2,13 +2,44 @@
 
 namespace rengage 
 {
-	std::unique_ptr<Shader> ShaderFactory::get_shader_from_file(const GLuint type, const std::string& filename)
+	std::unique_ptr<Shader> ShaderFactory::load_shader_from_file(const GLuint type, const char* filename)
 	{
+		std::ifstream input_file;
+		std::unique_ptr<Shader> shader_ptr = nullptr;
+
+		input_file.open(filename);
+		if (!input_file.good()) {
+			LOG_ERROR("Error opening file named '" + std::string {filename} + "'. Please check file or filename.")
+			return shader_ptr;
+		}
+
+		std::string shader_source;
+		std::string line;
+
+		while (!input_file.eof())
+		{
+			getline(input_file, line);
+			shader_source.append(line + "\n");
+		}
+		input_file.close();
+
+		auto shader_id = glCreateShader(type);
+		auto shader_source_ptr = shader_source.c_str();
+		compile_shader_source(shader_id, shader_source_ptr);
+
+		return shader_ptr;
+	}
+
+	std::unique_ptr<Shader> ShaderFactory::load_shader_from_source(const GLuint type, const char* source)
+	{
+		auto shader_id = glCreateShader(type);
+		compile_shader_source(shader_id, source);
 		return nullptr;
 	}
 
-	std::unique_ptr<Shader> ShaderFactory::get_shader_from_source(const GLuint type, const std::string& source)
+	GLuint ShaderFactory::compile_shader_source(const GLuint shader_id, const char* source)
 	{
-		return nullptr;
+		glShaderSource(shader_id, 1, &source, NULL);
+		glCompileShader(shader_id);
 	}
 }
