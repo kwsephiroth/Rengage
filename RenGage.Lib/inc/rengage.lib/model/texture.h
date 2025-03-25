c0d79f8c2c8dcbeee5f61a9423f73a908d40d2fd
Kenneth Walker <kwalkerdev@gmail.com>
2025-03-25 14:46:22 -0700

    Started work on a texture class.

--- RenGage.Lib/inc/rengage.lib/model/texture.h
+++ RenGage.Lib/inc/rengage.lib/model/texture.h
@@ -1,8 +1,27 @@
 #pragma once
+#include <GL/glew.h>
+#include <SOIL2/SOIL2.h>
+#include <string>
+#include "../logging/logger_macros.h"
+#include "../tools/ogl_invoker.h"
 
 namespace rengage::model {
 	class Texture
 	{
+	public:
+		Texture(const std::string& filename, std::shared_ptr<OGLInvoker> ogl_invoker, std::shared_ptr<ILogger> logger);
+		const std::string& filename() const { return m_filename; }
+		const GLuint handle() const { return m_handle; } 
+		const bool valid() const { return m_valid; }
+
+	private:
+		bool load();
+
+		std::string m_filename;
+		std::shared_ptr<OGLInvoker> m_ogl_invoker;
+		std::shared_ptr<ILogger> m_logger;
+		GLuint m_handle;// The Opengl texture handle
+		bool m_valid;
 	};
 }
 
--- RenGage.Lib/src/model/texture.cpp
+++ RenGage.Lib/src/model/texture.cpp
@@ -1,4 +1,39 @@
 #include "../../inc/rengage.lib/model/texture.h"
 
 namespace rengage::model {
+	Texture::Texture(const std::string& filename, std::shared_ptr<OGLInvoker> ogl_invoker, std::shared_ptr<ILogger> logger) :
+		m_filename(filename),
+		m_ogl_invoker(ogl_invoker),
+		m_logger(logger),
+		m_handle(0),
+		m_valid(false)
+	{
+		if (load())
+		{
+			m_valid = true;
+		}
+	}
+
+	bool Texture::load()
+	{
+		m_handle = SOIL_load_OGL_texture(m_filename.c_str(), SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_INVERT_Y);
+		if (m_handle == 0)
+		{
+			LOG_ERROR(m_logger, "Failed to load texture path '" + m_filename + "'.");
+			return false;
+		}
+
+		// ----- mipmap/anisotropic section
+		m_ogl_invoker->invoke(glBindTexture, ARGS(GL_TEXTURE_2D, m_handle));
+		m_ogl_invoker->invoke(glTexParameteri, ARGS(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR));
+		m_ogl_invoker->invoke(glGenerateMipmap, ARGS(GL_TEXTURE_2D));
+		if (glewIsSupported("GL_EXT_texture_filter_anisotropic")) {
+			GLfloat anisoset = 0.0f;
+			m_ogl_invoker->invoke(glGetFloatv, ARGS(GL_MAX_TEXTURE_MAX_ANISOTROPY_EXT, &anisoset));
+			m_ogl_invoker->invoke(glTexParameterf, ARGS(GL_TEXTURE_2D, GL_TEXTURE_MAX_ANISOTROPY_EXT, anisoset));
+		}
+		// ----- end of mipmap/anisotropic section
+
+		return true;
+	}
 }
